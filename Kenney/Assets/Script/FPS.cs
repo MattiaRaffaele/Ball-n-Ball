81aae7d970dc445c51c0e1591e94ad440b5773b9
MattiaRaffaele <raffaelemattia22@gmail.com>
2022-12-25 00:28:55 +0100

    backup

--- Kenney/Assets/Script/FPS.cs
+++ Kenney/Assets/Script/FPS.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class FPS : MonoBehaviour
 {
+    [Header("Framerate target")]
+    [Tooltip("FPS Count you would like to have")]
+    [SerializeField] int Fps;
+
     private void Start()
     {
-        Application.targetFrameRate = 180;
+        Application.targetFrameRate = Fps;
     }
 }
